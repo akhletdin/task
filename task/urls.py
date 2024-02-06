06833bcc66eed5e65c73e8ad3605121d09f9b535
akhletdin <ahletdinmacansin@gmail.com>
2024-02-06 22:54:45 +0600

    first commit

---
+++ task/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import TaskListCreate, TaskDetail
+
+urlpatterns = [
+    path('tasks/', TaskListCreate.as_view()),
+    path('tasks/<int:id>/', TaskDetail.as_view()),
+]
