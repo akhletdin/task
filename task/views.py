ca718596dd8095cdd9b059dfc21485911e2eae6e
akhletdin <ahletdinmacansin@gmail.com>
2024-02-06 23:12:29 +0600

    1

--- task/views.py
+++ task/views.py
@@ -1,14 +1,14 @@
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Task
 from .serializers import TaskSerializer
 
 
-class TaskListCreate(generics.ListCreateAPIView):
+class TaskListCreate(ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
 
-class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+class TaskDetail(RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     lookup_field = 'id'
